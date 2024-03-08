39a6760bda28dbea44ae7ec0af76f52caa65a5fb
heueun <kjh980615@naver.com>
2024-03-08 16:15:02 +0900

    [Refactor] 후기 작성 페이지 Vue.js 적용 (#99)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,16 @@ import MainPage from "@/pages/MainPage.vue";
 import AuthSignupPage from "@/pages/AuthSignupPage.vue";
 import SignupPage from '@/pages/SignupPage.vue';
 import BoardWritePage from '@/pages/BoardWritePage.vue';
+import ReviewWritePage from '@/pages/ReviewWritePage.vue';
+
 
 const routes = [
   { path: "/", component: MainPage },
   { path: '/authsignup', component: AuthSignupPage },
   { path: '/signup', component: SignupPage},
   { path: '/boardwrite', component: BoardWritePage},
+  { path: '/reviewwrite', component: ReviewWritePage},
+
 ];
 
 const router = createRouter({
