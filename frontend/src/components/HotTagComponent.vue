902e1d77a5a4d785052e0746125c3438dfde001c
hyeonjju <guswn2801@gmail.com>
2024-03-08 15:10:08 +0900

    [Refactor] 카테고리별 조회 페이지 Vue.js 적용 #101
    카테고리별 조회 페이지에서 사용하는 Hot Tag 컴포넌트 추가

---
+++ frontend/src/components/HotTagComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="css-19831hi"># 백엔드</div>
+</template>
+
+<script></script>
+
+<style>
+
+.css-19831hi {
+    width: 100%;
+    border-radius: 8px;
+    padding: 0px 16px;
+    /* 클릭하면 배경이 아래 색상으로 변경됨 */
+    background-color: rgb(242, 246, 248);
+    height: 48px;
+    font-family: Pretendard;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 48px;
+    font-weight: 700;
+    color: rgb(20, 22, 23);
+    cursor: pointer;
+}
+</style>
