395aaec02106194aab38fd2ee934d9ab7101020a
Tesssssssssy <ewoo97@naver.com>
2024-03-08 14:28:31 +0900

    [Refactor] 헤더, 푸터 컴포넌트 App.vue에 추가
    구현한 헤더, 푸터 컴포넌트 -> App.vue에 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-
-<div>ddddd</div>
+  <HeaderComponent />
+  <router-view></router-view>
+  <FooterComponent />
 </template>
 
 <script>
-
+import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 
 export default {
   name: "App",
   components: {
-
+    HeaderComponent,
+    FooterComponent
   },
 };
 </script>
