a9ae6ca81f1d5809ef5aa9a03f639af0d98215dc
Tesssssssssy <ewoo97@naver.com>
2024-03-08 15:30:39 +0900

    [Refactor] 검색 결과 페이지 Vue.js 구현 (#100)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
 import AuthSignupPage from "@/pages/AuthSignupPage.vue";
 import SignupPage from '@/pages/SignupPage.vue';
+import SearchResultPage from '@/pages/SearchResultPage.vue';
+
 
 const routes = [
   { path: "/", component: MainPage },
   { path: '/authsignup', component: AuthSignupPage },
-  { path: '/signup', component: SignupPage}
+  { path: '/signup', component: SignupPage},
+  { path: '/result', component: SearchResultPage}
 ];
 
 const router = createRouter({
