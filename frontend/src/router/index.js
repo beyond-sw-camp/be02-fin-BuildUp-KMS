aa51286ff4034cbc84ed1cd643c0b054ae45b5bc
gms <giljina333@naver.com>
2024-03-08 16:28:41 +0900

    [Chore] 유저 마이페이지 나의 활동 Router 등록 (#107)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
 import AuthSignupPage from "@/pages/AuthSignupPage.vue";
 import SignupPage from '@/pages/SignupPage.vue';
+import MyActivePage from "@/pages/MyActivePage.vue";
 
 const routes = [
   { path: "/", component: MainPage },
   { path: '/authsignup', component: AuthSignupPage },
-  { path: '/signup', component: SignupPage}
+  { path: '/signup', component: SignupPage},
+  { path: '/myactive', component: MyActivePage}
 ];
 
 const router = createRouter({
