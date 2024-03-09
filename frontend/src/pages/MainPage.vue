a46e083f99218f793a8f65e3996a0046062cfc7a
hyungdoyou <yhd4286@gmail.com>
2024-03-09 20:25:15 +0900

    [Rename] 페이지 명 및 라우터 URL이름 변경

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,28 +1,63 @@
 <template>
-  <HeaderComponent :showBorder="showBorder" :showMenu="showMenu" :showLogo="showLogo"/>
+  <HeaderComponent
+    :showBorder="showBorder"
+    :showMenu="showMenu"
+    :showLogo="showLogo"
+  />
   <div class="css-1hnxdb7">
     <div class="css-16z0wya">
       <section class="css-zea7jz0">
         <div class="ContentSearch_container__RFyCa">
-          <div class="SectionTitle_container__PcXfF SectionTitle_center__bSviY SectionTitle_size-48__RYzIo">
-            <p class="SectionTitle_label__cEYNl default SectionTitle_size-48__RYzIo"></p>
-            <p class="SectionTitle_title__iqOHl default SectionTitle_size-48__RYzIo">
+          <div
+            class="SectionTitle_container__PcXfF SectionTitle_center__bSviY SectionTitle_size-48__RYzIo"
+          >
+            <p
+              class="SectionTitle_label__cEYNl default SectionTitle_size-48__RYzIo"
+            ></p>
+            <p
+              class="SectionTitle_title__iqOHl default SectionTitle_size-48__RYzIo"
+            >
               <router-link to="/">
-                <img src="../assets/img/logo.png" alt="BOOKSHELF" width="500px" />
+                <img
+                  src="../assets/img/logo.png"
+                  alt="BOOKSHELF"
+                  width="500px"
+                />
               </router-link>
             </p>
-            <p class="SectionTitle_description__3Deqx default SectionTitle_size-48__RYzIo"></p>
-            <div class="SectionTitle_button__vZ_Mf SectionTitle_size-48__RYzIo"></div>
+            <p
+              class="SectionTitle_description__3Deqx default SectionTitle_size-48__RYzIo"
+            ></p>
+            <div
+              class="SectionTitle_button__vZ_Mf SectionTitle_size-48__RYzIo"
+            ></div>
           </div>
           <div class="ContentSearch_searchWrapper__3xeqG">
-            <input class="ContentSearch_searchInput__dVET9" id="content-search" type="search"
-              placeholder="Search here..." />
-            <span class="Icon_container__KMOfZ rotate-0 Icon_l-large__pSyZb Icon_m-large__BQ2ze"
-              style="--color: var(rgb(84, 29, 112))">
-              <svg width="40" height="40" viewBox="0 0 20 20" fill="purple" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
+            <input
+              class="ContentSearch_searchInput__dVET9"
+              id="content-search"
+              type="search"
+              placeholder="Search here..."
+            />
+            <span
+              class="Icon_container__KMOfZ rotate-0 Icon_l-large__pSyZb Icon_m-large__BQ2ze"
+              style="--color: var(rgb(84, 29, 112))"
+            >
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 20 20"
+                fill="purple"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
                   d="M2.45005 8.74999C2.45005 5.27059 5.27065 2.44999 8.75005 2.44999C12.2294 2.44999 15.05 5.27059 15.05 8.74999C15.05 12.2294 12.2294 15.05 8.75005 15.05C5.27065 15.05 2.45005 12.2294 2.45005 8.74999ZM8.75005 1.04999C4.49746 1.04999 1.05005 4.4974 1.05005 8.74999C1.05005 13.0026 4.49746 16.45 8.75005 16.45C10.6243 16.45 12.3421 15.7804 13.6774 14.6673L18.2551 19.245C18.5284 19.5183 18.9717 19.5183 19.245 19.245C19.5184 18.9716 19.5184 18.5284 19.245 18.255L14.6673 13.6773C15.7804 12.3421 16.45 10.6242 16.45 8.74999C16.45 4.4974 13.0026 1.04999 8.75005 1.04999Z"
-                  fill="purple" stroke="purple" stroke-width="2"></path>
+                  fill="purple"
+                  stroke="purple"
+                  stroke-width="2"
+                ></path>
               </svg>
             </span>
           </div>
@@ -42,8 +77,12 @@
                 집단 지성이 참여하고 <br />토론할 수 있는 문화
               </p>
             </div>
-            <img src="../assets/img/knowledge.png" loading="lazy" class="css-1522xt9" />
-          </button><button class="css-bcb9a9">
+            <img
+              src="../assets/img/knowledge.png"
+              loading="lazy"
+              class="css-1522xt9"
+            /></button
+          ><button class="css-bcb9a9">
             <div class="css-197aby8">
               <p class="css-1r2nemk">무엇이든 물어보세요</p>
               <p class="css-12j2poz">
@@ -51,8 +90,12 @@
                 공유하는 환경 제공
               </p>
             </div>
-            <img src="../assets/img/qna.png" loading="lazy" class="css-1522xt9" />
-          </button><button class="css-bcb9a9">
+            <img
+              src="../assets/img/qna.png"
+              loading="lazy"
+              class="css-1522xt9"
+            /></button
+          ><button class="css-bcb9a9">
             <div class="css-197aby8">
               <p class="css-1r2nemk">프로그램 참여 후기</p>
               <p class="css-12j2poz">
@@ -60,7 +103,11 @@
                 <br />공유하는 문화 형성
               </p>
             </div>
-            <img src="../assets/img/ddabong.png" loading="lazy" class="css-1522xt9" />
+            <img
+              src="../assets/img/ddabong.png"
+              loading="lazy"
+              class="css-1522xt9"
+            />
           </button>
         </div>
       </section>
@@ -71,39 +118,51 @@
               <p class="css-1r2nemk">인기 게시글</p>
               <p class="css-12j2poz">회원들의 HOT ISSUE</p>
             </div>
-            <img src="../assets/img/danger.png" loading="lazy" class="css-1522xt9" />
-          </button><button class="css-bcb9a9">
+            <img
+              src="../assets/img/danger.png"
+              loading="lazy"
+              class="css-1522xt9"
+            /></button
+          ><button class="css-bcb9a9">
             <div class="css-197aby8">
               <p class="css-1r2nemk">스터디원 모집</p>
               <p class="css-12j2poz">
                 함꼐 프로그래밍 공부를 할 <br />팀원을 모을 수 있는 환경
               </p>
             </div>
-            <img src="../assets/img/study.png" loading="lazy" class="css-1522xt9" />
-          </button><button class="css-bcb9a9">
+            <img
+              src="../assets/img/study.png"
+              loading="lazy"
+              class="css-1522xt9"
+            /></button
+          ><button class="css-bcb9a9">
             <div class="css-197aby8">
               <p class="css-1r2nemk">공지사항</p>
               <p class="css-12j2poz">BOOTSHELF 공지사항</p>
             </div>
-            <img src="../assets/img/notice.png" loading="lazy" class="css-1522xt9" />
+            <img
+              src="../assets/img/notice.png"
+              loading="lazy"
+              class="css-1522xt9"
+            />
           </button>
         </div>
       </section>
     </div>
   </div>
-
 </template>
 
 <script>
-import HeaderComponent from '@/components/HeaderComponent.vue';
+import HeaderComponent from "@/components/HeaderComponent.vue";
 
 export default {
+  name: "MainPage",
   components: {
     HeaderComponent,
   },
   data() {
     return {
-      showBorder: false, 
+      showBorder: false,
       showMenu: false,
       showLogo: false,
     };
@@ -602,7 +661,7 @@ textarea {
   /* 그림자 색상과 크기 설정 */
 }
 
-.Icon_container__KMOfZ+.ContentSearch_searchInput__dVET9 {
+.Icon_container__KMOfZ + .ContentSearch_searchInput__dVET9 {
   flex-grow: 1;
   color: rgb(84, 29, 112);
   /* 아이콘 색상 변경 */
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,38 +2,37 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import MainPage from "@/pages/MainPage.vue";
 import AuthSignupPage from "@/pages/AuthSignupPage.vue";
-import SignupPage from '@/pages/SignupPage.vue';
+import SignupPage from "@/pages/SignupPage.vue";
 import MyActivePage from "@/pages/MyActivePage.vue";
-import SearchResultPage from '@/pages/SearchResultPage.vue';
-import BoardWritePage from '@/pages/BoardWritePage.vue';
-import ReviewWritePage from '@/pages/ReviewWritePage.vue';
-import StudyBoardPage from '@/pages/StudyBoardPage.vue'
-import MainSignupPage from '@/pages/MainSignupPage.vue'
-import BoardDetailsPage from '@/pages/BoardDetailsPage.vue'
-import CategoryBoardPage from "@/pages/CategoryBoardPage.vue";
-import EmailValidationPage from '@/pages/EmailValidationPage.vue'
+import SearchResultPage from "@/pages/SearchResultPage.vue";
+import BoardWritePage from "@/pages/BoardWritePage.vue";
+import ReviewWritePage from "@/pages/ReviewWritePage.vue";
+import StudyBoardPage from "@/pages/StudyBoardPage.vue";
+import MainSignupPage from "@/pages/MainSignupPage.vue";
+import BoardDetailsPage from "@/pages/BoardDetailsPage.vue";
+import BoardListPage from "@/pages/BoardListPage.vue";
+import EmailValidationPage from "@/pages/EmailValidationPage.vue";
 import AdminMainPage from "@/pages/AdminMainPage.vue";
 
 const routes = [
   { path: "/", component: MainPage },
-  { path: '/authsignup', component: AuthSignupPage },
-  { path: '/signup', component: SignupPage},
-  { path: '/myactive', component: MyActivePage},
-  { path: '/result', component: SearchResultPage},
-  { path: '/boardwrite', component: BoardWritePage},
-  { path: '/reviewwrite', component: ReviewWritePage},
-  { path: '/study', component: StudyBoardPage},
-  { path: '/board/detail', component: BoardDetailsPage},
-  { path: '/categorylist', component: CategoryBoardPage},
-  { path: '/mainsignup', component: MainSignupPage},
-  { path: '/emailvalidation', component: EmailValidationPage},
-  { path: '/admin/', component: AdminMainPage},
+  { path: "/auth/signup", component: AuthSignupPage },
+  { path: "/signup", component: SignupPage },
+  { path: "/active", component: MyActivePage },
+  { path: "/result", component: SearchResultPage },
+  { path: "/board/new", component: BoardWritePage },
+  { path: "/review/new", component: ReviewWritePage },
+  { path: "/study", component: StudyBoardPage },
+  { path: "/board/detail", component: BoardDetailsPage },
+  { path: "/board", component: BoardListPage },
+  { path: "/select/signup", component: MainSignupPage },
+  { path: "/email/verify", component: EmailValidationPage },
+  { path: "/admin/", component: AdminMainPage },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
 });
-
 
 export default router;
