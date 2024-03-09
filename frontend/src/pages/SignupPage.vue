2fdcc8fda244a0e186b47d1b2e5ce20b6ec0f290
hyungdoyou <yhd4286@gmail.com>
2024-03-09 15:48:46 +0900

    [Refactor] 인증회원 회원가입 페이지 Vue.js 적용 (#104)

--- frontend/src/pages/SignupPage.vue
+++ frontend/src/pages/SignupPage.vue
@@ -1,167 +1,271 @@
 <template>
-    <div class="css-12gtq4k">
-      <div class="css-1k8rayr">
-        <div class="css-111ukc0">
-          <div class="css-1wlmsap">
-            <img src="@/assets/img/002.png" class="css-1k8svhy" />
-            <div class="line"></div>
-            <div direction="vertical" size="20" class="css-1i0k62c"></div>
-            <div class="pic-holder">
-              <!-- uploaded pic shown here -->
-              <img id="profilePic" class="pic" src="@/assets/img/profile.jpg" />
-              <input
-                class="uploadProfileInput"
-                type="file"
-                name="profile_pic"
-                id="newProfilePhoto"
-                accept="image/*"
-                style="opacity: 0"
-              />
-              <label for="newProfilePhoto" class="upload-file-block">
-                <div class="text-center">
-                  <div class="mb-2">
-                    <i class="fa fa-camera fa-2x"></i>
-                  </div>
-                  <div class="text-uppercase">
-                    프로필 이미지 <br />
-                    업로드
-                  </div>
+  <div class="css-12gtq4k">
+    <div class="css-1k8rayr">
+      <div class="css-111ukc0">
+        <div class="css-1wlmsap">
+          <img src="@/assets/img/002.png" class="css-1k8svhy" />
+          <div class="line"></div>
+          <div direction="vertical" size="20" class="css-1i0k62c"></div>
+          <div
+            class="pic-holder"
+            @mouseover="showUploadText = true"
+            @mouseleave="showUploadText = false"
+          >
+            <img
+              v-if="!selectedProfileImage"
+              class="pic"
+              :src="defaultProfileImage"
+            />
+            <img v-else class="pic" :src="selectedProfileImageURL" />
+            <input
+              class="uploadProfileInput"
+              type="file"
+              name="profile_pic"
+              id="newProfilePhoto"
+              accept="image/*"
+              style="opacity: 0"
+              @change="handleProfileImageChange"
+            />
+            <label
+              v-if="!selectedProfileImage || showUploadText"
+              for="newProfilePhoto"
+              class="upload-file-block"
+            >
+              <div class="text-center">
+                <div class="mb-2">
+                  <i class="fa fa-camera fa-2x"></i>
                 </div>
-              </label>
-            </div>
-            <div direction="vertical" size="60" class="css-1bisjhm"></div>
-            <div class="css-1b8vwo3">이메일</div>
-            <div class="css-666rgn">
-              <input
-                placeholder="이메일을 입력해 주세요."
-                type="string"
-                autocapitalize="off"
-                class="login-custom-input css-ownijh"
-                value=""
-              />
-            </div>
-            <div direction="vertical" size="20" class="css-1i0k62c"></div>
-
-            <div class="css-1b8vwo3-1">비밀번호</div>
-            <div class="css-666rgn">
-              <input
-                placeholder="비밀번호를 입력해주세요."
-                type="password"
-                autocapitalize="off"
-                class="login-custom-input css-1f4y3nx"
-                value=""
-              />
-            </div>
-            <div direction="vertical" size="20" class="css-1i0k62c"></div>
-            <div class="css-1b8vwo3-2">이름</div>
-            <div class="css-666rgn">
-              <input
-                placeholder="이름을 입력해주세요."
-                type="string"
-                autocapitalize="off"
-                class="login-custom-input css-ownijh"
-                value=""
-              />
-            </div>
-            <div direction="vertical" size="20" class="css-1i0k62c"></div>
-
-            <div class="css-1b8vwo3-3">닉네임</div>
-            <div class="css-666rgn">
-              <input
-                placeholder="닉네임을 입력해주세요."
-                type="string"
-                autocapitalize="off"
-                class="login-custom-input css-ownijh"
-                value=""
-              />
-            </div>
-            <div class="css-17w7nyr"></div>
-            <div class="css-8luw5u">
-              <button class="css-27eumk">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                <div
+                  class="text-uppercase"
+                  v-if="!selectedProfileImage || showUploadText"
                 >
-                  <rect
-                    x="3.33331"
-                    y="3.33331"
-                    width="13.3333"
-                    height="13.3333"
-                    rx="2"
-                    stroke="#9DA7AE"
-                    stroke-width="1.75"
-                  ></rect>
-                </svg>
-                전체 약관 동의
-              </button>
-              <div class="css-1jf7ho2">
-                <div class="css-d650kt">
-                  <button class="css-7hwvbl"></button>
-                </div>
-                <div class="css-d650kt">
-                  <button class="css-7hwvbl">
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M3.33301 10L7.49967 14.1667L16.6663 5"
-                        stroke="#B4BFC6"
-                        stroke-width="1.75"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></path>
-                    </svg>
-                    <div class="css-w2z16z">
-                      [필수] 통합 서비스 이용약관
-                    </div></button
-                  ><a class="css-1rvdgmi">보기</a>
-                </div>
-                <div class="css-d650kt">
-                  <button class="css-7hwvbl">
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M3.33301 10L7.49967 14.1667L16.6663 5"
-                        stroke="#B4BFC6"
-                        stroke-width="1.75"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></path>
-                    </svg>
-                    <div class="css-w2z16z">
-                      [필수] 개인정보 처리방침
-                    </div></button
-                  ><a class="css-1rvdgmi">보기</a>
-                </div>
-                <div class="css-d650kt">
-                  <button class="css-7hwvbl"></button>
+                  프로필 이미지 <br />
+                  업로드
                 </div>
               </div>
-            </div>
-            <div direction="vertical" size="40" class="css-ygt1wz"></div>
-            <button color="#FFFFFF" class="css-j27xag">가입하기</button>
+            </label>
           </div>
+          <!-----------------프로필 이미지 업로드----------------->
+          <div direction="vertical" size="60" class="css-1bisjhm"></div>
+          <div class="css-1b8vwo3">이메일</div>
+          <div class="css-666rgn">
+            <input
+              placeholder="이메일을 입력해 주세요."
+              type="string"
+              autocapitalize="off"
+              class="login-custom-input css-ownijh"
+              value=""
+            />
+          </div>
+          <div direction="vertical" size="20" class="css-1i0k62c"></div>
+
+          <div class="css-1b8vwo3-1">비밀번호</div>
+          <div class="css-666rgn">
+            <input
+              placeholder="비밀번호를 입력해주세요."
+              type="password"
+              autocapitalize="off"
+              class="login-custom-input css-1f4y3nx"
+              value=""
+            />
+          </div>
+          <div direction="vertical" size="20" class="css-1i0k62c"></div>
+          <div class="css-1b8vwo3-2">이름</div>
+          <div class="css-666rgn">
+            <input
+              placeholder="이름을 입력해주세요."
+              type="string"
+              autocapitalize="off"
+              class="login-custom-input css-ownijh"
+              value=""
+            />
+          </div>
+          <div direction="vertical" size="20" class="css-1i0k62c"></div>
+
+          <div class="css-1b8vwo3-3">닉네임</div>
+          <div class="css-666rgn">
+            <input
+              placeholder="닉네임을 입력해주세요."
+              type="string"
+              autocapitalize="off"
+              class="login-custom-input css-ownijh"
+              value=""
+            />
+          </div>
+          <div direction="vertical" size="32" class="css-h23ofx"></div>
+          <div class="css-17w7nyr"></div>
+          <div class="css-8luw5u">
+            <button class="css-27eumk" @click="toggleAllAgreements">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="3.33331"
+                  y="3.33331"
+                  width="13.3333"
+                  height="13.3333"
+                  rx="2"
+                  :fill="allAgreements ? '#141617' : 'none'"
+                  :stroke="allAgreements ? '#141617' : '#9DA7AE'"
+                  stroke-width="1.75"
+                ></rect>
+                <path
+                  v-if="allAgreements"
+                  d="M6.8327 10.2727L8.91604 12.1667L13.4994 8"
+                  stroke="#FFFFFF"
+                  stroke-width="1.25"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+              전체 약관 동의
+            </button>
+            <div class="css-1jf7ho2">
+              <div
+                v-for="(agreement, index) in agreements"
+                :key="index"
+                class="css-d650kt"
+              >
+                <button class="css-7hwvbl" @click="toggleAgreement(index)">
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      v-if="!agreement.checked"
+                      d="M3.33301 10L7.49967 14.1667L16.6663 5"
+                      stroke="#B4BFC6"
+                      stroke-width="1.75"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                    <path
+                      v-if="agreement.checked"
+                      d="M3.33301 10L7.49967 14.1667L16.6663 5"
+                      stroke="#141617"
+                      stroke-width="1.75"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                  <div class="css-w2z16z">{{ agreement.label }}</div>
+                </button>
+                <a class="css-1rvdgmi">보기</a>
+              </div>
+            </div>
+          </div>
+          <div direction="vertical" size="40" class="css-ygt1wz"></div>
+          <button color="#FFFFFF" class="css-j27xag">가입하기</button>
         </div>
       </div>
     </div>
+  </div>
 </template>
+
 <script>
 export default {
-  name: 'SignupPage',
-}
+  name: "AuthSignupPage",
+  data() {
+    return {
+      defaultProfileImage: require("@/assets/img/profile.jpg"),
+      selectedProfileImage: null,
+      selectedProfileImageURL: "",
+      showUploadText: false,
+
+      isUploading: false,
+      isUploaded: false,
+      uploadProgress: 0,
+      uploadedFile: null,
+      errorMessage: "",
+
+      isOpenGuide: false,
+
+      allAgreements: false,
+      agreements: [
+        { label: "[필수] 만 14세 이상", checked: false },
+        { label: "[필수] 서비스 약관 동의", checked: false },
+        { label: "[필수] 개인정보처리방침 및 제3자 제공 동의", checked: false },
+        { label: "[선택] 광고성 정보 수신 동의", checked: false },
+      ],
+    };
+  },
+  mounted() {
+    this.$root.hideHeaderAndFooter = true;
+  },
+  methods: {
+    handleProfileImageChange(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.selectedProfileImage = file;
+        this.selectedProfileImageURL = URL.createObjectURL(file);
+        this.showUploadText = false; // 이미지가 선택되면 텍스트를 숨깁니다.
+      }
+    },
+
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      const ext = file.name.split(".").pop().toLowerCase();
+
+      if (["gif", "png", "jpg", "jpeg"].indexOf(ext) === -1) {
+        this.errorMessage = "이미지 파일이 아닙니다.";
+      } else {
+        this.errorMessage = "";
+        this.isUploading = true;
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          setTimeout(() => {
+            this.uploadedFile = reader.result;
+            this.isUploaded = true;
+            this.isUploading = false;
+          }, 3000);
+        };
+
+        reader.onprogress = (e) => {
+          if (e.lengthComputable) {
+            this.uploadProgress = Math.round((e.loaded / e.total) * 100);
+          }
+        };
+
+        reader.readAsDataURL(file);
+      }
+    },
+    removeUploadedFile() {
+      this.uploadedFile = null;
+      this.isUploaded = false;
+      this.uploadProgress = 0;
+    },
+
+    openGuideModal() {
+      this.isOpenGuide = true;
+    },
+    closeGuideModal() {
+      this.isOpenGuide = false;
+    },
+
+    toggleAllAgreements() {
+      this.allAgreements = !this.allAgreements;
+      this.agreements.forEach(
+        (agreement) => (agreement.checked = this.allAgreements)
+      );
+    },
+    toggleAgreement(index) {
+      this.agreements[index].checked = !this.agreements[index].checked;
+      this.allAgreements = this.agreements.every(
+        (agreement) => agreement.checked
+      );
+    },
+  },
+};
 </script>
+
 <style scoped>
 body {
   display: block;
@@ -222,14 +326,6 @@ element.style {
   justify-content: space-between;
   -webkit-box-flex: 1;
   flex-grow: 1;
-}
-
-path {
-  d: path("M 9.16699 5 L 4.16699 10 L 9.16699 15");
-  stroke: rgb(58, 62, 65);
-  stroke-width: 1.75;
-  stroke-linecap: round;
-  stroke-linejoin: round;
 }
 
 :not(svg) {
@@ -354,6 +450,7 @@ input:not(
   width: 100%;
   height: 1px;
   background: #eef3f6;
+  margin-top: 70px;
 }
 
 .css-8luw5u {
@@ -362,7 +459,6 @@ input:not(
   flex-direction: column;
   gap: 20px;
   align-items: flex-start;
-  margin-top: 50px;
 }
 
 a,
@@ -425,13 +521,7 @@ button {
   cursor: pointer;
 }
 
-path {
-  d: path("M 3.33301 10 L 7.49967 14.1667 L 16.6663 5");
-  stroke: rgb(180, 191, 198);
-  stroke-width: 1.75;
-  stroke-linecap: round;
-  stroke-linejoin: round;
-}
+/*----------체크박스 추가------------*/
 
 svg {
   width: 20;
@@ -545,10 +635,6 @@ svg {
 /* 이미지 첨부 버튼 */
 
 /*---------------------------------*/
-* {
-  font-family: "Metro Sans", sans-serif;
-  box-sizing: border-box;
-}
 
 .css-1b8vwo3::after {
   position: relative;
@@ -631,36 +717,6 @@ svg {
 
 .text {
   text-decoration: underline;
-}
-
-/* ---------- 이미지 첨부 버튼-------------*/
-._17nutjM9EW1e6w7K8cCfKS {
-  text-align: center;
-  margin-top: 10px;
-  margin-right: 300px;
-}
-
-.btn-primary {
-  display: inline-block;
-  cursor: pointer;
-  font-weight: 300;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  user-select: none;
-  border: 1px solid transparent;
-  padding: 0.3125rem 0.75rem;
-  font-size: 0.875rem;
-  line-height: 1.714286;
-  border-radius: 20px;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-.btn-primary {
-  color: #fff;
-  background-color: #609ae9;
-  border-color: #609ae9;
 }
 /*-------------프로필 이미지 업로드---------------*/
 .h1 {
@@ -803,5 +859,4 @@ svg {
   }
 }
 /*-------------프로필 이미지 업로드---------------*/
-
-</style>./SignUpPage.vue
+</style>
