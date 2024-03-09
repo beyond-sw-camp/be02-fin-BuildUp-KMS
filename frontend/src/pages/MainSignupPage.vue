17301ba9a7c0aaa385caffce9343e5bfe53c812d
heueun <kjh980615@naver.com>
2024-03-09 13:16:37 +0900

    [Refactor] 공통 회원가입 페이지 Vue.js 적용 (#104)

---
+++ frontend/src/pages/MainSignupPage.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="message-container">
+      <img src="@/assets/img/002.png" class="css-1k8svhy">
+      <div class="container">
+        <h2>어떤 회원으로 가입하시겠습니까?</h2>
+        <div class="container-2">
+          <div class="button-container">
+            <div class="description">
+              <p>
+                일반회원은 국비지원교육, 부트캠프를
+                <br>
+                한 번도 수강한 이력이 없는
+                회원입니다.&nbsp;&nbsp;&nbsp;&nbsp;
+              </p>
+              <button class="button normal-member">
+                일반 회원가입
+              </button>
+            </div>
+          </div>
+          <br>
+          <div class="button-container">
+            <div class="description">
+              <p>
+                인증회원은 최근 2년 간 국비지원교육, 부트캠프를
+                <br>
+                한 번이라도 수강한 이력이 있는 회원입니다.
+              </p>
+              &nbsp;&nbsp;
+              <button class="button corporate-member">
+                인증 회원가입
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'MainSignupPage',
+}
+</script>
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  margin: 1px auto;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.container-2 {
+  margin: 90px auto;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+h2 {
+  color: #333;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.button {
+  display: inline-block;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-right: 10px;
+
+  /* 테두리 스타일 제거 */
+  border: none;
+
+  /* 둥근 테두리 설정 */
+  border-radius: 8px;
+}
+
+.normal-member {
+  background-color: #5bb629;
+  color: #fff;
+  font-weight: bold;
+  box-shadow: rgb(0 0 0 / 15%) 7px 5px 8px;
+}
+
+.corporate-member {
+  background-color: #541d7a;
+  color: #fff;
+  font-weight: bold;
+  box-shadow: rgb(0 0 0 / 15%) 7px 5px 8px;
+}
+
+.description {
+  flex-grow: 0.1; /* 오른쪽에 공간을 차지하도록 설정 */
+  text-align: left;
+  padding-left: 20px; /* 오른쪽 여백을 설정하여 내용을 가운데 정렬하지 않게 함 */
+  display: flex;
+  justify-content: space-between;
+  max-width: 1000px;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+h2 {
+  color: #333;
+  margin-bottom: 20px;
+}
+.css-1k8svhy {
+  width: 100%; /* 이미지를 부모 요소의 너비에 맞게 조정 */
+  max-width: 300px; /* 최대 너비를 248px로 제한 */
+  height: auto; /* 가로 비율에 맞게 세로 크기 자동 조정 */
+  object-fit: contain;
+  margin-top: 120px;
+}
+
+</style>
