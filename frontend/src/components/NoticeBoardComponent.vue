f809844d2b404679653d3993e21cdd83a82a8d40
hyungdoyou <yhd4286@gmail.com>
2024-03-10 11:49:35 +0900

    [Refactor] 공지사항 게시글 Vue.js 적용을 위한 컴포넌트 전환 (#101)

---
+++ frontend/src/components/NoticeBoardComponent.vue
@@ -0,0 +1,263 @@
+<template>
+  <div class="css-1agvh3p">
+    <div class="css-1x8gxrp">
+      <div class="css-s3qw6k">
+        <span class="css-0">[공지] 팀스파르타 개인정보 처리방침 개정 안내</span
+        ><span class="css-1wp0bsh"></span><span class="css-1hkyyfd">N</span>
+      </div>
+    </div>
+    <div class="css-ktviac">
+      <div class="css-1p269h3">
+        <span class="css-1d3nptd">24.02.16</span>
+      </div>
+      <div class="css-wh2oxb">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6.70711 9.29289C6.31658 8.90237 5.68342 8.90237 5.29289 9.29289C4.90237 9.68342 4.90237 10.3166 5.29289 10.7071L6.70711 9.29289ZM12 16L11.2929 16.7071C11.6834 17.0976 12.3166 17.0976 12.7071 16.7071L12 16ZM18.7071 10.7071C19.0976 10.3166 19.0976 9.68342 18.7071 9.29289C18.3166 8.90237 17.6834 8.90237 17.2929 9.29289L18.7071 10.7071ZM5.29289 10.7071L11.2929 16.7071L12.7071 15.2929L6.70711 9.29289L5.29289 10.7071ZM12.7071 16.7071L18.7071 10.7071L17.2929 9.29289L11.2929 15.2929L12.7071 16.7071Z"
+            fill="#141617"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="css-173r1bj">
+    <p>안녕하세요.</p>
+    <p>
+      스파르타코딩클럽을 비롯한 팀스파르타의 서비스를 이용해주시는 여러분들께
+      진심으로 감사드립니다.
+    </p>
+    <p><br /></p>
+    <p>팀스파르타의 개인정보 처리방침 개정 관련 공지드립니다.</p>
+    <p><br /></p>
+    <p>
+      <strong style="color: rgb(58, 62, 65)"
+        >개인정보 처리방침 변경 안내</strong
+      >
+    </p>
+    <p>
+      팀스파르타의 개인정보 처리방침이 아래와 같이 일부 변경되어 2024년 02월
+      23일 부로 시행됩니다.
+    </p>
+    <p><br /></p>
+    <p><strong>[변경내용]</strong></p>
+    <p>1) 개정 내용</p>
+    <p>
+      a. 제1조(수집하는 개인정보 항목 및 수집방법) 중 '부트캠프 취업지원 서비스
+      이용 시' 내용 추가
+    </p>
+    <p>
+      b. 제6조(개인정보의 처리위탁) 중 '부트캠프 취업지원 서비스' 관련 제3자
+      제공 내용 추가
+    </p>
+    <p><br /></p>
+    <p>
+      2) 개정 사유:&nbsp;<strong>부트캠프 취업지원 서비스 제공에</strong
+      ><strong style="color: rgb(80, 82, 84)">
+        따른 개인정보 수집 및 활용 안내를 위함</strong
+      >
+    </p>
+    <p><br /></p>
+    <p><u>3) 시행 일자: 2024년 02월 23일</u></p>
+    <p>
+      <em
+        >* 시행일까지 회원이 별도의 동의 또는 거부의 의사표시를 하지 않을 경우,
+        승낙한 것으로 간주됩니다.</em
+      >
+    </p>
+    <p><br /></p>
+    <p>
+      <a
+        href="https://teamsparta.notion.site/7b1dc644460946f08bab08b794de685f"
+        rel="noopener noreferrer"
+        target="_blank"
+        style="color: rgb(17, 152, 227)"
+        >4) 개인정보 처리방침 확인하</a
+      ><a
+        href="https://teamsparta.notion.site/7b1dc644460946f08bab08b794de685f"
+        rel="noopener noreferrer"
+        target="_blank"
+        >기</a
+      >
+    </p>
+    <p><br /></p>
+    <p>
+      관련하여 궁금하신 사항은 홈페이지 우측 하단 [문의하기]를 이용해주세요.
+    </p>
+    <p><br /></p>
+    <p>
+      <span style="color: rgb(80, 82, 84)"
+        >언제나 팀스파르타 서비스를 사랑해주시고 이용해주셔서 진심으로
+        감사합니다.</span
+      >
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "NoticeBoardComponent",
+};
+</script>
+<style scoped>
+.css-1agvh3p {
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  gap: 24px;
+  width: 100%;
+  cursor: pointer;
+}
+
+@media (min-width: 768px) {
+  .css-1agvh3p {
+    gap: 40px;
+  }
+}
+.css-1x8gxrp {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: calc(100% - 44px);
+  margin-left: -140px;
+}
+
+.css-s3qw6k {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+  color: rgb(58, 62, 65);
+  font-family: __pretendard_1fdc56, __pretendard_Fallback_1fdc56;
+  white-space: pre-wrap;
+  align-items: center;
+  display: inline-block;
+}
+
+@media (min-width: 768px) {
+  .css-s3qw6k {
+    display: unset;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    color: rgb(58, 62, 65);
+    font-family: __pretendard_1fdc56, __pretendard_Fallback_1fdc56;
+  }
+}
+
+.css-1wp0bsh {
+  width: 0px;
+  height: 100%;
+  margin-right: 6px;
+}
+
+@media (min-width: 768px) {
+  .css-1wp0bsh {
+    margin-right: 12px;
+  }
+}
+
+.css-1hkyyfd {
+  background: rgb(232, 52, 78);
+  display: inline-block;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  border-radius: 4px;
+  color: rgb(255, 255, 255);
+  width: 18px;
+  height: 18px;
+  font-size: 10px;
+  font-weight: 800;
+  flex-shrink: 0;
+  line-height: 18px;
+  text-align: center;
+}
+
+@media (min-width: 768px) {
+  .css-1hkyyfd {
+    font-size: 12px;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+  }
+}
+
+.css-ktviac {
+  display: flex;
+  gap: 40px;
+  width: fit-content;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.css-1d3nptd {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 150%;
+  color: rgb(180, 191, 198);
+  font-family: __pretendard_1fdc56, __pretendard_Fallback_1fdc56;
+}
+
+@media (min-width: 768px) {
+  .css-1d3nptd {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    color: rgb(180, 191, 198);
+    font-family: __pretendard_1fdc56, __pretendard_Fallback_1fdc56;
+  }
+}
+
+.css-wh2oxb {
+  cursor: pointer;
+  transition: all 0.4s ease 0s;
+}
+
+.css-173r1bj {
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  color: rgb(129, 137, 143);
+  height: fit-content;
+  overflow: hidden;
+  transition: all 0.4s ease 0s;
+  max-height: 0px;
+  padding: 24px 44px 0px 0px;
+  font-size: 14px;
+}
+
+@media (min-width: 768px) {
+  .css-173r1bj {
+    padding: 32px 64px 0px 0px;
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+    color: rgb(129, 137, 143);
+    font-size: 16px;
+  }
+}
+
+.css-173r1bj p {
+  color: rgb(95, 102, 107);
+  line-height: 150%;
+  font-weight: 500;
+}
+
+.css-173r1bj strong {
+  font-weight: 700;
+  color: rgb(58, 62, 65) !important;
+}
+</style>
