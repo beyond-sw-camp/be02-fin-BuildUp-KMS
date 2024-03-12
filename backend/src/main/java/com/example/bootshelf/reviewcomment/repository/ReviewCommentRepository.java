e2c3823eace2b348033a119cd9f437f16d15aee0
hyeonjju <guswn2801@gmail.com>
2024-03-12 11:54:05 +0900

    [Feat] 후기글 댓글 조회 기능 개발 완료 #56
    후기글 댓글 조회 기능 완료

--- backend/src/main/java/com/example/bootshelf/reviewcomment/repository/ReviewCommentRepository.java
+++ backend/src/main/java/com/example/bootshelf/reviewcomment/repository/ReviewCommentRepository.java
@@ -4,6 +4,11 @@ import com.example.bootshelf.reviewcomment.model.entity.ReviewComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Integer> {
+
+    List<ReviewComment> findByReviewIdx(Integer reviewIdx);
+
 }
