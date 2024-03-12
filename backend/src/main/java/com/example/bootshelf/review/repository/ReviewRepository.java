4fb15adf71a439e1765ddd499efc162de0e97154
Tesssssssssy <ewoo97@naver.com>
2024-03-12 09:11:33 +0900

    [Feat] 백엔드 리뷰 스크랩 생성 조회 기능 구현 (진행중) (#40)

--- backend/src/main/java/com/example/bootshelf/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/example/bootshelf/review/repository/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.example.bootshelf.review.model.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    Optional<Review> findByIdx(Integer idx);
 }
