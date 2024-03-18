96491ff3b0047398ae2b2aae4fcb572e5fe75d33
Tesssssssssy <ewoo97@naver.com>
2024-03-18 09:02:48 +0900

    [Feat] 프론트엔드 관리자 게시판 카테고리 수정 페이지 vue.js 구현 (#146)

--- frontend/src/stores/useBoardStore.js
+++ frontend/src/stores/useBoardStore.js
@@ -329,5 +329,25 @@ export const useBoardStore = defineStore("board", {
         console.log(e);
       }
     },
+
+    async deleteBoardCategory(boardCategoryIdx) {
+      try {
+        await axios.delete(backend + "/admin/board/delete/" + boardCategoryIdx);
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+
+    async updateBoardCategory(boardCategoryIdx, newCategoryName) {
+      try {
+        await axios.patch(backend + "/admin/board/update/" + boardCategoryIdx, {
+          categoryName: newCategoryName
+        })
+      } catch(e) {
+        console.error(e);
+        throw e;
+      }
+    }
   },
 });
