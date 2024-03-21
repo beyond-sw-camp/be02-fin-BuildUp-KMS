1ffa463b61a31061b24fadbc53005305db13540f
Tesssssssssy <ewoo97@naver.com>
2024-03-21 19:39:49 +0900

    [CI/CD] develop 브랜치 푸쉬할 때 백엔드 파이프라인만 실행 테스트 1

---
+++ backend/src/test/java/com/example/bootshelf/reviewsvc/reviewup/controller/ReviewUpControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.bootshelf.reviewsvc.reviewup.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewUpControllerTest {
+
+    @Test
+    void createReviewUp() {
+    }
+}
