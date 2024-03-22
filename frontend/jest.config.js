3cbae3a380035371ccc260a1b1835fdf243ee67c
heueun <kjh980615@naver.com>
2024-03-22 09:43:54 +0900

    [Test] 테스트 환경 설정

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,24 @@
 const config = {
-  preset: '@vue/cli-plugin-unit-jest',
-  testMatch :  ["<rootDir>/tests/unit/**/*.spec.js"],
   reporters: [
     'default',
     ['jest-junit', {outputDirectory: 'test-results', outputName: 'report.xml'}],
   ],
+  preset: '@vue/cli-plugin-unit-jest',
+  testMatch: [
+    '**/__tests__/**/*.spec.[jt]s?(x)',
+    '**/tests/unit/**/*.spec.[jt]s?(x)'
+  ],
+  moduleNameMapper: {
+    '^/stores/userStore$': '<rootDir>/src/stores/userStore.js',
+    '^pinia$': '<rootDir>/node_modules/pinia',
+    '^axios$': '<rootDir>/node_modules/axios',
+  },
+  transformIgnorePatterns: [
+    '^/node_modules/(?!axios)',
+    '^/node_modules/(?!pinia)',
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
 };
 module.exports = config;
--- frontend/tests/unit/reviewCreateTest.spec.js
+++ frontend/tests/unit/reviewCreateTest.spec.js
@@ -1,8 +1,5 @@
-import { shallowMount, flushPromises } from '@vue/test-utils';
-import { createPinia, setActivePinia } from 'pinia';
-import { createRouter, createWebHistory } from 'vue-router';
-import ReviewWritePage from '@/pages/ReviewWritePage.vue'; 
-import { useReviewStore } from '@/stores/useReviewStore'; 
+import { shallowMount } from '@vue/test-utils';
+import ReviewWritePage from '@/components/ReviewWritePage.vue';
 
 jest.mock('frontend/src/stores/useReviewStore', () => ({
     useReviewStore: jest.fn().mockReturnValue({
@@ -10,7 +7,33 @@ jest.mock('frontend/src/stores/useReviewStore', () => ({
     }),
 }));
 
-const routes = [
-    { path: '/', component: { template: '<div>Home</div>' } },
-    { path: '/review/new/register', name: 'admin-tag-register', component: { template: '<div>관리자 태그 등록</div>' } }
-];
+
+
+describe('ReviewWritePage', () => {
+
+    let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ReviewWritePage);
+  });
+
+  it('toggles isClicked when showCategory is called', async () => {
+    expect(wrapper.vm.isClicked).toBe(false);
+    await wrapper.vm.showCategory();
+    expect(wrapper.vm.isClicked).toBe(true);
+  });
+
+  it('selects category and updates reviewCategoryIdx', async () => {
+    await wrapper.vm.selectCategory({ name: '과정 후기', selected: false });
+    expect(wrapper.vm.selectedCategory).toBe('과정 후기');
+    expect(wrapper.vm.review.reviewCategoryIdx).toBe(1);
+  });
+
+  it('toggles isEvaluationClicked when showEvaluation is called', async () => {
+    expect(wrapper.vm.isEvaluationClicked).toBe(false);
+    await wrapper.vm.showEvaluation();
+    expect(wrapper.vm.isEvaluationClicked).toBe(true);
+  });
+
+});
+
