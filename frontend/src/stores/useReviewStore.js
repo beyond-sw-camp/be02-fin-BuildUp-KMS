4e9972594a8c718bb7e6499889e139cad0483021
gms <giljina333@naver.com>
2024-03-27 11:34:56 +0900

    [Test] 게시판 게시글 등록 테스트코드 진행중 (mearged)

--- frontend/src/stores/useReviewStore.js
+++ frontend/src/stores/useReviewStore.js
@@ -1,8 +1,8 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-const backend = "http://192.168.0.61/api";
-// const backend = "http://localhost:8080";
+// const backend = "http://192.168.0.61/api";
+const backend = "http://localhost:8080";
 const storedToken = localStorage.getItem("token");
 
 export const useReviewStore = defineStore("review", {
