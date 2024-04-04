a5f52c409cfc2c7d00dd4be15fd576e2f1fb5b6e
hyungdoyou <yhd4286@gmail.com>
2024-04-04 10:59:37 +0900

    [Test] Refresh Token CI/CD Test

--- backend/src/main/java/com/example/bootshelf/esreview/model/response/ReviewSearchRes.java
+++ backend/src/main/java/com/example/bootshelf/esreview/model/response/ReviewSearchRes.java
@@ -1,27 +1,27 @@
-package com.example.bootshelf.esreview.model.response;
-
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class ReviewSearchRes {
-    private Integer idx;
-    private Integer user;
-    private String reviewTitle;
-    private String reviewContent;
-    private Integer reviewCategory;
-    private String nickName;
-    private String createdAt;
-    private Integer viewCnt;
-    private Integer commentCnt;
-    private Integer upCnt;
-    private Integer scrapCnt;
-    private String updatedAt;
-    private Boolean status;
-    private Long totalHits;
-    private String courseName;
-    private Integer courseEvaluation;
-}
+//package com.example.bootshelf.esreview.model.response;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class ReviewSearchRes {
+//    private Integer idx;
+//    private Integer user;
+//    private String reviewTitle;
+//    private String reviewContent;
+//    private Integer reviewCategory;
+//    private String nickName;
+//    private String createdAt;
+//    private Integer viewCnt;
+//    private Integer commentCnt;
+//    private Integer upCnt;
+//    private Integer scrapCnt;
+//    private String updatedAt;
+//    private Boolean status;
+//    private Long totalHits;
+//    private String courseName;
+//    private Integer courseEvaluation;
+//}
