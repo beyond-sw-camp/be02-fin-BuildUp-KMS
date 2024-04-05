14666b695f6793ab617b72f7ebc8083c8247b225
hyeonjju <guswn2801@gmail.com>
2024-04-05 12:16:56 +0900

    es axios 연결 및 검색어 하이라이팅 진행 중

--- frontend/src/components/CategoryBoardComponent.vue
+++ frontend/src/components/CategoryBoardComponent.vue
@@ -11,23 +11,21 @@
           <div class="css-12i5occ">
             <div class="css-1jibmi3">
               <!--게시글 타이틀-->
-              <div class="css-cp47oo">
-                {{ boards.title }}
+              <!-- <div class="css-cp47oo">
+                {{ boards.boardTitle }}
               </div>
               <div class="css-14bssip">
-                {{ boards.content }}
-              </div>
+                {{ boards.boardContent }}
+              </div> -->
+              <div class="css-cp47oo">{{ highlightedTitle }}</div>
+              <div class="css-14bssip">{{ highlightedContent }}</div>
             </div>
             <!--태그 컴포넌트 자리-->
-            <TagComponent :tagNameList="boards.tagNameList" />
+            <TagComponent :tagNameList="boards.tagName" />
           </div>
           <div class="css-bt1qy"></div>
           <!--사진-->
-          <img
-            v-if="boards.boardImg"
-            :src="boards.boardImg"
-            class="css-17s6wd5"
-          />
+          <img v-if="boards.boardImg" :src="boards.boardImg" class="css-17s6wd5" />
         </div>
         <!--여기서부터는 하단 아이디,추천수..등등-->
         <div class="css-99cwur">
@@ -35,8 +33,7 @@
           <div class="css-1fhge30">
             <div class="css-aw18wm">
               <!-- 아이콘-->
-              <span
-                style="
+              <span style="
                   box-sizing: border-box;
                   display: block;
                   overflow: hidden;
@@ -49,14 +46,8 @@
                   padding: 0px;
                   position: absolute;
                   inset: 0px;
-                "
-              >
-                <img
-                  sizes="100vw"
-                  :src="boards.userProfileImage"
-                  decoding="async"
-                  data-nimg="fill"
-                  style="
+                ">
+                <img sizes="100vw" :src="boards.userProfileImage" decoding="async" data-nimg="fill" style="
                     position: absolute;
                     inset: 0px;
                     box-sizing: border-box;
@@ -70,21 +61,14 @@
                     max-width: 100%;
                     min-height: 100%;
                     max-height: 100%;
-                  "
-                />
+                  " />
               </span>
             </div>
             <div class="css-5zcuov">
               <div class="css-1sika4i">{{ boards.nickName }}</div>
               <div class="css-1tify6w">
                 <!--조그만 점 찍기-->
-                <svg
-                  width="2"
-                  height="2"
-                  viewBox="0 0 2 2"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg width="2" height="2" viewBox="0 0 2 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="1" cy="1" r="1" fill="#9DA7AE"></circle>
                 </svg>
               </div>
@@ -120,41 +104,13 @@
         <div class="css-192oc4s">
           <div class="css-ts29it">
             <div class="css-hr47l6">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M8 13.999L7.364 14.3965C7.50106 14.6158 7.74141 14.749 8 14.749C8.25859 14.749 8.49895 14.6158 8.636 14.3965L8 13.999ZM9.47083 11.6457L8.83483 11.2482L9.47083 11.6457ZM3.33333 3.41565H12.6667V1.91565H3.33333V3.41565ZM13.25 3.99898V9.99898H14.75V3.99898H13.25ZM2.75 9.99898V3.99898H1.25V9.99898H2.75ZM8.636 13.6015L7.16517 11.2482L5.89317 12.0431L7.364 14.3965L8.636 13.6015ZM5.96384 10.5823H3.33333V12.0823H5.96384V10.5823ZM12.6667 10.5823H10.0362V12.0823H12.6667V10.5823ZM8.83483 11.2482L7.364 13.6015L8.636 14.3965L10.1068 12.0431L8.83483 11.2482ZM10.0362 10.5823C9.54771 10.5823 9.09371 10.8339 8.83483 11.2482L10.1068 12.0431C10.0916 12.0675 10.0649 12.0823 10.0362 12.0823V10.5823ZM1.25 9.99898C1.25 11.1496 2.18274 12.0823 3.33333 12.0823V10.5823C3.01117 10.5823 2.75 10.3211 2.75 9.99898H1.25ZM13.25 9.99898C13.25 10.3211 12.9888 10.5823 12.6667 10.5823V12.0823C13.8173 12.0823 14.75 11.1496 14.75 9.99898H13.25ZM12.6667 3.41565C12.9888 3.41565 13.25 3.67682 13.25 3.99898H14.75C14.75 2.84839 13.8173 1.91565 12.6667 1.91565V3.41565ZM7.16517 11.2482C6.90629 10.8339 6.45229 10.5823 5.96384 10.5823V12.0823C5.9351 12.0823 5.9084 12.0675 5.89317 12.0431L7.16517 11.2482ZM3.33333 1.91565C2.18274 1.91565 1.25 2.84839 1.25 3.99898H2.75C2.75 3.67682 3.01117 3.41565 3.33333 3.41565V1.91565Z"
-                  fill="#B4BFC6"
-                ></path>
-                <rect
-                  x="4.58203"
-                  y="6.25"
-                  width="1.5"
-                  height="1.5"
-                  rx="0.75"
-                  fill="#B4BFC6"
-                ></rect>
-                <rect
-                  x="7.25"
-                  y="6.25"
-                  width="1.5"
-                  height="1.5"
-                  rx="0.75"
-                  fill="#B4BFC6"
-                ></rect>
-                <rect
-                  x="9.91797"
-                  y="6.25"
-                  width="1.5"
-                  height="1.5"
-                  rx="0.75"
-                  fill="#B4BFC6"
-                ></rect>
+                  fill="#B4BFC6"></path>
+                <rect x="4.58203" y="6.25" width="1.5" height="1.5" rx="0.75" fill="#B4BFC6"></rect>
+                <rect x="7.25" y="6.25" width="1.5" height="1.5" rx="0.75" fill="#B4BFC6"></rect>
+                <rect x="9.91797" y="6.25" width="1.5" height="1.5" rx="0.75" fill="#B4BFC6"></rect>
               </svg>
             </div>
             <div class="css-i21m7n">{{ boards.commentCnt }}</div>
@@ -163,26 +119,12 @@
         <div class="css-192oc4s">
           <div class="css-ts29it">
             <div class="css-hr47l6">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="2"
-                  y="6.66565"
-                  width="1.33333"
-                  height="6"
-                  stroke="#B4BFC6"
-                  stroke-width="1.5"
-                  stroke-linejoin="round"
-                ></rect>
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="2" y="6.66565" width="1.33333" height="6" stroke="#B4BFC6" stroke-width="1.5"
+                  stroke-linejoin="round"></rect>
                 <path
                   d="M9.1972 2.16336L8.62103 1.68322L9.1972 2.16336ZM10.9725 11.9167H6.66667V13.4167H10.9725V11.9167ZM6.75 12V6.96547H5.25V12H6.75ZM13.25 6.66667V8.91304H14.75V6.66667H13.25ZM13.1446 9.24756L11.4503 11.6679L12.6792 12.528L14.3734 10.1078L13.1446 9.24756ZM11.3333 6.75H13.3333V5.25H11.3333V6.75ZM7.19424 5.73845L9.77336 2.6435L8.62103 1.68322L6.04191 4.77817L7.19424 5.73845ZM9.25 2.45402V4.66667H10.75V2.45402H9.25ZM9.54598 2.75C9.38252 2.75 9.25 2.61748 9.25 2.45402H10.75C10.75 1.78906 10.2109 1.25 9.54598 1.25V2.75ZM9.77336 2.6435C9.71713 2.71098 9.63382 2.75 9.54598 2.75V1.25C9.18866 1.25 8.84979 1.40872 8.62103 1.68322L9.77336 2.6435ZM11.3333 5.25C11.0112 5.25 10.75 4.98883 10.75 4.66667H9.25C9.25 5.81726 10.1827 6.75 11.3333 6.75V5.25ZM13.25 8.91304C13.25 9.03272 13.2132 9.14951 13.1446 9.24756L14.3734 10.1078C14.6185 9.75758 14.75 9.34048 14.75 8.91304H13.25ZM6.66667 11.9167C6.71269 11.9167 6.75 11.954 6.75 12H5.25C5.25 12.7824 5.88426 13.4167 6.66667 13.4167V11.9167ZM10.9725 13.4167C11.6523 13.4167 12.2893 13.085 12.6792 12.528L11.4503 11.6679C11.3412 11.8238 11.1628 11.9167 10.9725 11.9167V13.4167ZM14.75 6.66667C14.75 5.88426 14.1157 5.25 13.3333 5.25V6.75C13.2873 6.75 13.25 6.71269 13.25 6.66667H14.75ZM6.75 6.96547C6.75 6.51709 6.9072 6.0829 7.19424 5.73845L6.04191 4.77817C5.53022 5.3922 5.25 6.16618 5.25 6.96547H6.75Z"
-                  fill="#B4BFC6"
-                ></path>
+                  fill="#B4BFC6"></path>
               </svg>
             </div>
             <div class="css-i21m7n">{{ boards.upCnt }}</div>
@@ -191,17 +133,10 @@
         <div class="css-192oc4s">
           <div class="css-ts29it">
             <div class="css-hr47l6">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M12.6677 14L12.2621 14.6309C12.4929 14.7792 12.7863 14.7898 13.0271 14.6583C13.2679 14.5268 13.4177 14.2744 13.4177 14H12.6677ZM3.33435 14H2.58435C2.58435 14.2744 2.73415 14.5268 2.97496 14.6583C3.21576 14.7898 3.50914 14.7792 3.73992 14.6309L3.33435 14ZM8.00102 11L8.40659 10.3691C8.15953 10.2103 7.8425 10.2103 7.59545 10.3691L8.00102 11ZM4.66768 2.75H11.3344V1.25H4.66768V2.75ZM11.9177 3.33333V14H13.4177V3.33333H11.9177ZM4.08435 14V3.33333H2.58435V14H4.08435ZM13.0733 13.3691L8.40659 10.3691L7.59545 11.6309L12.2621 14.6309L13.0733 13.3691ZM7.59545 10.3691L2.92878 13.3691L3.73992 14.6309L8.40659 11.6309L7.59545 10.3691ZM11.3344 2.75C11.6565 2.75 11.9177 3.01117 11.9177 3.33333H13.4177C13.4177 2.18274 12.4849 1.25 11.3344 1.25V2.75ZM4.66768 1.25C3.51709 1.25 2.58435 2.18274 2.58435 3.33333H4.08435C4.08435 3.01117 4.34552 2.75 4.66768 2.75V1.25Z"
-                  fill="#B4BFC6"
-                ></path>
+                  fill="#B4BFC6"></path>
               </svg>
             </div>
             <div class="css-i21m7n">{{ boards.scrapCnt }}</div>
@@ -226,10 +161,22 @@ export default {
   components: {
     TagComponent,
   },
+  methods: {
+    highlight(boards) {
+      // 게시글 제목과 내용에 대한 하이라이팅된 내용이 boards 객체의 속성으로 포함되어 있다고 가정
+      // 예시로 직접 지정
+      this.highlightedTitle = boards.boardTitle; // 게시글 제목
+      this.highlightedContent = boards.boardContent; // 게시글 내용
+    }
+  }
 };
 </script>
 
 <style scoped>
+em {
+  color: red;
+}
+
 .css-k59gj9 {
   display: -webkit-box;
   display: -webkit-flex;
@@ -240,6 +187,7 @@ export default {
   flex-direction: column;
   width: 100%;
 }
+
 @media (min-width: 820px) {
   .css-aw1sgr {
     padding: 15px 0px;
@@ -249,6 +197,7 @@ export default {
     padding-right: 0px;
   }
 }
+
 .css-aw1sgr {
   display: flex;
   flex-direction: column;
@@ -262,6 +211,7 @@ export default {
 .css-aw1sgr:hover {
   background-color: rgb(242, 246, 248);
 }
+
 .css-amlmv6 {
   width: 100%;
   display: none;
@@ -272,11 +222,13 @@ export default {
   align-items: center;
   gap: 8px;
 }
+
 @media (min-width: 820px) {
   .css-amlmv6 {
     display: none;
   }
 }
+
 .css-1254q6y {
   display: inline-flex;
   -webkit-box-pack: start;
@@ -299,6 +251,7 @@ export default {
     margin-left: 8px;
   }
 }
+
 .css-kem115 {
   display: flex;
   flex-direction: row;
@@ -308,18 +261,21 @@ export default {
   align-items: center;
   gap: 14px;
 }
+
 .css-12i5occ {
   display: flex;
   flex-direction: column;
   gap: 8px;
   width: 100%;
 }
+
 @media (min-width: 820px) {
   .css-12i5occ {
     gap: 4px;
     width: 100%;
   }
 }
+
 .css-1jibmi3 {
   display: -webkit-box;
   display: -webkit-flex;
@@ -330,6 +286,7 @@ export default {
   flex-direction: column;
   gap: 4px;
 }
+
 .css-cp47oo {
   overflow: hidden;
   text-overflow: ellipsis;
@@ -357,6 +314,7 @@ export default {
     font-weight: 600;
   }
 }
+
 @media (min-width: 820px) {
   .css-14bssip {
     overflow: hidden;
@@ -390,6 +348,7 @@ export default {
   word-break: keep-all;
   height: 45px;
 }
+
 .css-sebsp7 {
   display: flex;
   flex-direction: row;
@@ -405,6 +364,7 @@ export default {
   -webkit-box-align: start;
   align-items: start;
 }
+
 .css-1ry6usa {
   font-family: Pretendard;
   font-style: normal;
@@ -413,6 +373,7 @@ export default {
   line-height: 18px;
   color: rgb(157, 167, 174);
 }
+
 .css-99cwur {
   display: flex;
   flex-direction: row;
@@ -423,6 +384,7 @@ export default {
   height: 24px;
   margin-right: 20px;
 }
+
 .css-1fhge30 {
   display: flex;
   flex-direction: row;
@@ -430,6 +392,7 @@ export default {
   align-items: center;
   gap: 8px;
 }
+
 .css-aw18wm {
   width: 24px;
   height: 24px;
@@ -437,6 +400,7 @@ export default {
   border-radius: 100%;
   overflow: hidden;
 }
+
 .css-5zcuov {
   display: -webkit-box;
   display: -webkit-flex;
@@ -451,6 +415,7 @@ export default {
   -ms-flex-align: center;
   align-items: center;
 }
+
 .css-5zcuovs {
   display: -webkit-box;
   display: -webkit-flex;
@@ -471,6 +436,7 @@ export default {
   background-color: rgba(84, 29, 112, 0.218);
   padding: 3px 10px;
 }
+
 .css-1sika4i {
   font-family: Pretendard;
   font-style: normal;
@@ -479,6 +445,7 @@ export default {
   line-height: 18px;
   color: rgb(58, 62, 65);
 }
+
 .css-1sika4is {
   font-family: Pretendard;
   font-style: normal;
@@ -487,11 +454,13 @@ export default {
   line-height: 18px;
   color: rgb(58, 62, 65);
 }
+
 .css-1tify6w {
   width: 2px;
   height: 2px;
   display: flex;
 }
+
 @media (min-width: 820px) {
   .css-o01lup {
     display: flex;
@@ -501,9 +470,11 @@ export default {
     gap: 2px;
   }
 }
+
 .css-o01lup {
   display: none;
 }
+
 .css-ts29it {
   display: flex;
   flex-direction: row;
@@ -511,6 +482,7 @@ export default {
   align-items: center;
   gap: 4px;
 }
+
 .css-dbc8ke {
   font-family: Pretendard;
   font-style: normal;
@@ -526,6 +498,7 @@ export default {
     display: none;
   }
 }
+
 .css-1vkj2s1 {
   flex-direction: row;
   -webkit-box-align: center;
@@ -533,6 +506,7 @@ export default {
   gap: 4px;
   display: flex;
 }
+
 .css-z2xt5y {
   width: 100%;
   display: none;
@@ -548,6 +522,7 @@ export default {
     display: flex;
   }
 }
+
 .css-k9ergi {
   width: 100%;
   height: 42px;
@@ -560,6 +535,7 @@ export default {
   justify-content: space-between;
   padding: 0px 24px;
 }
+
 .css-192oc4s {
   display: flex;
   flex-direction: row;
@@ -570,10 +546,12 @@ export default {
   width: 96px;
   height: 42px;
 }
+
 .css-hr47l6 {
   width: 16px;
   height: 16px;
 }
+
 .css-i21m7n {
   font-family: Pretendard;
   font-style: normal;
@@ -582,6 +560,7 @@ export default {
   line-height: 18px;
   color: rgb(180, 191, 198);
 }
+
 @media (min-width: 820px) {
   .css-17t7asl {
     display: -webkit-box;
@@ -595,11 +574,13 @@ export default {
     gap: 24px;
   }
 }
+
 @media (min-width: 820px) {
   .css-k9ergi {
     display: none;
   }
 }
+
 @media (min-width: 820px) {
   .css-o01lup {
     display: flex;
@@ -615,11 +596,13 @@ export default {
     display: none;
   }
 }
+
 @media (min-width: 820px) {
   .css-z2xt5y {
     display: flex;
   }
 }
+
 .css-17s6wd5 {
   width: 100px;
   height: 92px;
@@ -637,6 +620,7 @@ img {
   image-rendering: -webkit-optimize-contrast;
   -ms-interpolation-mode: nearest-neighbor;
 }
+
 a {
   color: rgb(157, 167, 174);
   text-decoration: none;
@@ -652,6 +636,7 @@ a:hover {
   gap: 10px;
   padding-top: 10px;
 }
+
 .css-17s6wd5 {
   width: 92px;
   height: 92px;
@@ -661,6 +646,7 @@ a:hover {
   display: flex;
   flex-shrink: 0;
 }
+
 .css-updatedTimeGms {
   font-size: 10px;
   color: rgb(157, 167, 174);
