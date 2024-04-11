a8a54221de0a7dd988112d4dbdf1534c92463829
hyungdoyou <yhd4286@gmail.com>
2024-04-11 09:30:25 +0900

    [Test] 프론트엔드 테스트를 위한 통합테스트 코드 주석 처리 (#589)

--- frontend/tests/integration/signupNickNameFailed.spec.js
+++ frontend/tests/integration/signupNickNameFailed.spec.js
@@ -1,72 +1,72 @@
-import { describe, expect, test, beforeAll, afterAll } from "@jest/globals";
-const { Builder, By, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+// import { describe, expect, test, beforeAll, afterAll } from "@jest/globals";
+// const { Builder, By, until } = require("selenium-webdriver");
+// const chrome = require("selenium-webdriver/chrome");
 
-describe("일반회원 회원가입 테스트", () => {
-  let driver;
-  beforeAll(async () => {
-    driver = await new Builder()
-      .forBrowser("chrome")
-      .setChromeOptions(
-        new chrome.Options().addArguments("--headless") // 크롬을 띄우지 않도록 설정
-      )
-      .build();
+// describe("일반회원 회원가입 테스트", () => {
+//   let driver;
+//   beforeAll(async () => {
+//     driver = await new Builder()
+//       .forBrowser("chrome")
+//       .setChromeOptions(
+//         new chrome.Options().addArguments("--headless") // 크롬을 띄우지 않도록 설정
+//       )
+//       .build();
 
-    await driver.get("http://192.168.0.61/signup");
+//     await driver.get("http://192.168.0.61/signup");
 
-    await driver.wait(() =>
-      driver
-        .executeScript("return document.readyState")
-        .then((readyState) => readyState === "complete")
-    );
-  });
+//     await driver.wait(() =>
+//       driver
+//         .executeScript("return document.readyState")
+//         .then((readyState) => readyState === "complete")
+//     );
+//   });
 
-  afterAll(async () => {
-    await driver.quit();
-  }, 40000);
+//   afterAll(async () => {
+//     await driver.quit();
+//   }, 40000);
 
-  test("회원가입 실패 케이스 테스트 - 닉네임 중복", async () => {
+//   test("회원가입 실패 케이스 테스트 - 닉네임 중복", async () => {
 
-    const uniqueData = `test${Date.now()}@example.com`;
+//     const uniqueData = `test${Date.now()}@example.com`;
 
-    const input_email = await driver.wait(
-      until.elementLocated(By.id("custEmail")),
-    );
-    await input_email.sendKeys(uniqueData);
+//     const input_email = await driver.wait(
+//       until.elementLocated(By.id("custEmail")),
+//     );
+//     await input_email.sendKeys(uniqueData);
 
-    const input_pw = await driver.wait(
-      until.elementLocated(By.id("custPw")),
-    );
-    await input_pw.sendKeys("Tester01!");
+//     const input_pw = await driver.wait(
+//       until.elementLocated(By.id("custPw")),
+//     );
+//     await input_pw.sendKeys("Tester01!");
 
-    const input_name = await driver.wait(
-      until.elementLocated(By.id("custName")),
-    );
-    await input_name.sendKeys("test01");
+//     const input_name = await driver.wait(
+//       until.elementLocated(By.id("custName")),
+//     );
+//     await input_name.sendKeys("test01");
 
-    const input_nickname = await driver.wait(
-      until.elementLocated(By.id("custNickName")),
-    );
-    await input_nickname.sendKeys("test01");
+//     const input_nickname = await driver.wait(
+//       until.elementLocated(By.id("custNickName")),
+//     );
+//     await input_nickname.sendKeys("test01");
 
-    const agree_btn = await driver.wait(
-      until.elementLocated(By.id("agree")),
-    );
-    await agree_btn.click();
+//     const agree_btn = await driver.wait(
+//       until.elementLocated(By.id("agree")),
+//     );
+//     await agree_btn.click();
 
-    const signup_btn = await driver.wait(
-      until.elementLocated(By.id("signupBtn")),
-    );
-    await signup_btn.click();
+//     const signup_btn = await driver.wait(
+//       until.elementLocated(By.id("signupBtn")),
+//     );
+//     await signup_btn.click();
 
-    await driver.wait(until.alertIsPresent());
+//     await driver.wait(until.alertIsPresent());
 
-    let alert = await driver.switchTo().alert();
+//     let alert = await driver.switchTo().alert();
 
-    let alertText = await alert.getText();
+//     let alertText = await alert.getText();
 
-    await alert.accept();
+//     await alert.accept();
 
-    expect(alertText).toContain("이미 사용중인 닉네임입니다. 닉네임을 변경해주세요.");
-  });
-});
+//     expect(alertText).toContain("이미 사용중인 닉네임입니다. 닉네임을 변경해주세요.");
+//   });
+// });
