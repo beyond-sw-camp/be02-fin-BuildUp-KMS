ae7a0c2a588678d3cf2607f634555ae58e600143
heueun <kjh980615@naver.com>
2024-04-22 03:45:25 +0900

    [Chore] 비밀번호 입력값 검증 수정

--- backend/src/main/java/com/example/bootshelf/user/model/request/PostSignUpUserReq.java
+++ backend/src/main/java/com/example/bootshelf/user/model/request/PostSignUpUserReq.java
@@ -22,7 +22,7 @@ public class PostSignUpUserReq {
 
     @NotNull(message = "패스워드는 필수 입력 항목입니다.")
     @Length(min=8, max=45 , message = "패스워드는 8글자 이상, 45글자 이하여야 합니다.")
-    @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,45}$", message = "패스워드는 대/소문자, 특수문자, 숫자를 반드시 포함한 8글자 이상이어야 합니다.")
+    @Pattern(regexp="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\\\",.<>/?]).{8,}$", message = "패스워드는 대/소문자, 특수문자, 숫자를 반드시 포함한 8글자 이상이어야 합니다.")
     @ApiModelProperty(value = "패스워드(최소 : 8글자, 최대 45글자 / 적어도 한개의 대/소문자, 특수문자, 숫자를 포함)", example = "Qwer1234@", required = true)
     private String password;
 
